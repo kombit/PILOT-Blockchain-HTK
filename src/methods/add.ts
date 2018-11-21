0a443d4a7a1bf3a77644c5ea009d91d2a192c753
johncoffee <github2017@ragelse.dk>
2018-11-21 18:41:58 +0100

    use join

--- src/methods/add.ts
+++ src/methods/add.ts
@@ -1,8 +1,10 @@
+import { join } from 'path'
+
 const Web3 = require('web3')
 
 export async function add (address:string, subcontractAddress:string, from:string) {
   const web3 = new Web3('http://localhost:7545')
-  const instance:any = new web3.eth.Contract(require('../../ethereum/build/contracts/IHasSubcontracts.json').abi,
+  const instance:any = new web3.eth.Contract(require(join(__dirname,'../../ethereum/build/contracts/IHasSubcontracts.json')).abi,
     address, {
       from: from,
     })
@@ -10,7 +12,7 @@ export async function add (address:string, subcontractAddress:string, from:strin
   instance.methods.add(subcontractAddress)
     .send()
     .then(() => {
-      const instance:any = new web3.eth.Contract(require('../../ethereum/build/contracts/ICommonState.json').abi,
+      const instance:any = new web3.eth.Contract(require(join(__dirname,'../../ethereum/build/contracts/ICommonState.json')).abi,
         address,
         {})
       instance.methods.getSubcontract(0).call().then(val => {
