b81edb6da1956d296462c93baf6c0cb8c62ec5c2
johncoffee <github2017@ragelse.dk>
2018-12-04 15:26:55 +0100

    use k4 as interface

--- src/methods/step.ts
+++ src/methods/step.ts
@@ -1,12 +1,12 @@
 import { getWeb3 } from '../web3.js'
 import { join } from "path"
 
-export async function step (contractName:string, n:string, address:string, from:string) {
+export async function step (num:string, address:string, from:string) {
   const web3 = getWeb3()
 
-  const instance = new web3.eth.Contract(require(join(__dirname, `../../ethereum/build/contracts/${contractName}.json`)).abi,
+  const instance = new web3.eth.Contract(require(join(__dirname, `../../ethereum/build/contracts/K4.json`)).abi,
     address,
     {from:from})
 
-  await instance.methods.step(n).send()
+  await instance.methods.step(num).send()
 }
